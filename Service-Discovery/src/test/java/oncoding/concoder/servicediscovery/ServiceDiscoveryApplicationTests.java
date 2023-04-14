205666f5d30ce46e9bb89a740f0f3abb37a37694
BambooKim <rlaqjarn1008@gmail.com>
2023-04-14 15:37:58 +0900

    Initial Commit

---
+++ Service-Discovery/src/test/java/oncoding/concoder/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package oncoding.concoder.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
